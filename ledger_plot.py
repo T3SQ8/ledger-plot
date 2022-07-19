8030ce2fa0dd5e58e2fe768d3d0b188947375449
T3SQ8 <h9s@tutanota.com>
2022-07-19 22:54:39 +0200

    Added options using argparse

--- ledger_plot.py
+++ ledger_plot.py
@@ -2,50 +2,62 @@
 
 import sys
 import subprocess
+import argparse
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-LEDGER = 'ledger'
+def ledger_fetch_data(command):
+    print(' '.join(command), file=sys.stderr)
+    stdout = subprocess.check_output(command).decode().split('\n')
+    stdout = list(filter(None, stdout)) # Remove empty items
+    return stdout
 
-def ledger_fetch_data(ledger_files):
-    ledger_command = [LEDGER, 'reg']
-    for file in ledger_files:
+def process_line(transaction):
+    date, amount = transaction.split()
+    date = datetime.strptime(date, '%Y-%m-%d')
+    amount = float(amount)
+    return date, amount
+
+def main():
+    parser = argparse.ArgumentParser(description='Visually plot ledger files.')
+    parser.add_argument('ledger_files', nargs='+', metavar='FILE', help='Input ledger file(s).')
+    # Limit timeframes specified to only one
+    timeframe_arg = parser.add_mutually_exclusive_group()
+    timeframe_arg.add_argument('-D', '--daily', dest='timeframe', action='store_const', const='-D',
+            default='-D', # Setting the default value
+            help='Group postings by day. (Default)')
+    timeframe_arg.add_argument('-W', '--weekly', dest='timeframe', action='store_const', const='-W',
+            help='Group postings by week (starting on Sundays).')
+    timeframe_arg.add_argument('-M', '--monthly', dest='timeframe', action='store_const',
+            const='-M',
+            help='Group postings by month.')
+    timeframe_arg.add_argument('--quarterly', dest='timeframe', action='store_const',
+            const='--quarterly',
+            help='Group postings by fiscal quarter.')
+    timeframe_arg.add_argument('-Y', '--yearly', dest='timeframe', action='store_const', const='-Y',
+            help='Group postings by year.')
+    args = parser.parse_args()
+
+    ledger_command = ['ledger', 'reg', args.timeframe]
+    for file in args.ledger_files:
         ledger_command.extend(['-f', file])
 
     ledger_queue = {
-            'Monthly cashflow':     ledger_command + ['-M', '-j', '-n', '^income', '^expenses'],
-            #'Daily asset total':    ledger_command + ['-D', '-J', '-n', '^assets'],
-            #'Monthly asset growth': ledger_command + ['-M', '-j', '-n', '^assets'],
-            'Monthly expenditure':  ledger_command + ['-M', '-j', '-n', '^expenses'],
-            'Daily networth':       ledger_command + ['-D', '-J', '-n', '^assets', '^liabilities'],
-            #'Monthly networth':     ledger_command + ['-M', '-J', '-n', '^assets', '^liabilities'],
+            'Cashflow':     ledger_command + ['-j', '-n', '^income', '^expenses'],
+            'Asset total':  ledger_command + ['-J', '-n', '^assets'],
+            'Asset growth': ledger_command + ['-j', '-n', '^assets'],
+            'Expenditure':  ledger_command + ['-j', '-n', '^expenses'],
+            'Networth':     ledger_command + ['-J', '-n', '^assets', '^liabilities'],
     }
 
     for category, command in ledger_queue.items():
-        print(' '.join(command))
-        # Grab STDOUT and split into list.
-        command_output = subprocess.check_output(command).decode().split('\n')
-        command_output = list(filter(None, command_output)) # Remove empty items
-        dates = []
-        amounts = []
-        for transaction in command_output:
-            date, amount = transaction.split()
-            dates.append(datetime.strptime(date, '%Y-%m-%d'))
-            amounts.append(float(amount))
-        ledger_queue[category] = (dates, amounts)
+        dates, amounts = [], []
+        for transaction in ledger_fetch_data(command):
+            date, amount = process_line(transaction)
+            dates.append(date)
+            amounts.append(amount)
+        plt.plot(dates, amounts, label=category)
 
-    return ledger_queue
-
-def main():
-    ledger_files = sys.argv[1:]
-    if not ledger_files:
-        print('No files given')
-        sys.exit(1)
-
-    data = ledger_fetch_data(ledger_files)
-
-    for category, items in data.items():
-        plt.plot(items[0], items[1], label=category)
 
     plt.legend()
     plt.show()
